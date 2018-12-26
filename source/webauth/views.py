8c8255b42a25fe336fd4ac3917684800c9e30a6e
galubezzz galubezzz <galubezzzgalubezzz@galubezzzs-MacBook-Air.local>
2018-12-26 15:15:17 -0800

    added view for user's login

---
+++ source/webauth/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth import login, logout, authenticate
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        next = request.POST.get('next')
+        user = authenticate(request, username=username, password=password)
+        if user is None:
+            context = {
+                'username': username,
+                'password': password,
+                'next': next,
+                'has_error': True
+            }
+            return render(request, 'login.html', context=context)
+        else:
+            login(request, user=user)
+            redirect_url = next
+            if not redirect_url:
+                redirect_url = reverse('webapp:project_list')
+            return HttpResponseRedirect(redirect_url)
+
+    return render(request, 'login.html', context={'next': request.GET.get('next')})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
